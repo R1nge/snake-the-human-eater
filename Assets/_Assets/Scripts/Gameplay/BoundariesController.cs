64a5efed6b9fe48d07a301184472f1b8442b97b2
R1nge <r1ngegamedev@gmail.com>
2024-06-12 21:27:00 +0300

    Player screen boundaries

---
+++ Assets/_Assets/Scripts/Gameplay/BoundariesController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace _Assets.Scripts.Gameplay
+{
+    public class BoundariesController
+    {
+        private readonly Transform _playerTransform;
+        private readonly Camera _camera;
+
+        public BoundariesController(Transform playerTransform, Camera camera)
+        {
+            _playerTransform = playerTransform;
+            _camera = camera;
+        }
+
+        public void Update()
+        {
+            var viewportPosition = _camera.WorldToViewportPoint(_playerTransform.position);
+            
+            if (viewportPosition.x > 1 || viewportPosition.x < 0 || viewportPosition.y > 1 || viewportPosition.y < 0)
+            {
+                viewportPosition.x = viewportPosition.x > 1 ? 0 : viewportPosition.x < 0 ? 1 : viewportPosition.x;
+                viewportPosition.y = viewportPosition.y > 1 ? 0 : viewportPosition.y < 0 ? 1 : viewportPosition.y;
+                
+                _playerTransform.position = _camera.ViewportToWorldPoint(viewportPosition);
+            }
+        }
+    }
+}
--- Assets/_Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/_Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using _Assets.Scripts.Services.Gameplay;
+﻿using System;
+using _Assets.Scripts.Services.Gameplay;
 using UnityEngine;
 using VContainer;
 
@@ -7,10 +8,17 @@ namespace _Assets.Scripts.Gameplay
     public class PlayerController : MonoBehaviour
     {
         [Inject] private InputService _inputService;
+        private BoundariesController _boundariesController;
+
+        private void Start()
+        {
+            _boundariesController = new BoundariesController(transform, Camera.main);
+        }
 
         private void Update()
         {
             transform.position += new Vector3(_inputService.Horizontal, _inputService.Vertical) * Time.deltaTime;
+            _boundariesController.Update();
         }
     }
 }
