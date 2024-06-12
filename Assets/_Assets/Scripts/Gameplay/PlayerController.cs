3c0789e393aa2e520fd328f923003b087606e78d
R1nge <r1ngegamedev@gmail.com>
2024-06-12 21:30:46 +0300

    Refactored player movement

--- Assets/_Assets/Scripts/Gameplay/PlayerController.cs
+++ Assets/_Assets/Scripts/Gameplay/PlayerController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using _Assets.Scripts.Services.Gameplay;
+﻿using _Assets.Scripts.Services.Gameplay;
 using UnityEngine;
 using VContainer;
 
@@ -8,16 +7,18 @@ namespace _Assets.Scripts.Gameplay
     public class PlayerController : MonoBehaviour
     {
         [Inject] private InputService _inputService;
+        private PlayerMovement _playerMovement;
         private BoundariesController _boundariesController;
 
         private void Start()
         {
+            _playerMovement = new PlayerMovement(transform, _inputService, Time.deltaTime);
             _boundariesController = new BoundariesController(transform, Camera.main);
         }
 
         private void Update()
         {
-            transform.position += new Vector3(_inputService.Horizontal, _inputService.Vertical) * Time.deltaTime;
+            _playerMovement.Move();
             _boundariesController.Update();
         }
     }
---
+++ Assets/_Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using _Assets.Scripts.Services.Gameplay;
+using UnityEngine;
+
+namespace _Assets.Scripts.Gameplay
+{
+    public class PlayerMovement
+    {
+        private readonly Transform _transform;
+        private readonly InputService _inputService;
+        private readonly float _delta;
+
+        public PlayerMovement(Transform transform, InputService inputService, float delta)
+        {
+            _transform = transform;
+            _inputService = inputService;
+            _delta = delta;
+        }
+        
+        public void Move()
+        {
+            _transform.position += new Vector3(_inputService.Horizontal, _inputService.Vertical) * _delta;
+        }
+    }
+}
